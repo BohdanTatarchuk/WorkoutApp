8437c6e07b46c2ba60c6f960c6e70cddfbc9e128
Bohdan Tatarchuk <102388122+BohdanTatarchuk@users.noreply.github.com>
2022-05-06 22:56:30 +0200

    Adding the blocks of exercises

--- app/src/main/java/com/example/absworkout/MainActivity.java
+++ app/src/main/java/com/example/absworkout/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.absworkout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView block;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        block  = findViewById(R.id.item1);
+        block.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(String.valueOf(MainActivity.this), "CLick");
+            }
+        });
     }
 }
