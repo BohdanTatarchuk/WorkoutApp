89d84c9f6f47e995b43d347f96904b47b5587dda
Bohdan Tatarchuk <102388122+BohdanTatarchuk@users.noreply.github.com>
2022-05-07 23:18:56 +0200

    Timer works properly.

--- app/src/main/java/com/example/absworkout/Timer.java
+++ app/src/main/java/com/example/absworkout/Timer.java
@@ -2,13 +2,68 @@ package com.example.absworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Timer extends AppCompatActivity {
+    TextView time;
+    ImageView play, stop;
+
+    CountDownTimer timer;
+    boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        time = findViewById(R.id.time);
+        play = findViewById(R.id.play);
+        stop = findViewById(R.id.stop);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimer();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               pauseTimer();
+            }
+        });
+    }
+
+    private void startTimer(){
+        timer = new CountDownTimer(60*1000, 1000) {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onTick(long l) {
+                time.setText(Long.toString((l/1000)));
+            }
+
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+            }
+        }.start();
+        timerRunning = true;
+    }
+
+    private void pauseTimer(){
+        timer.cancel();
+        timerRunning = false;
+    }
+
+    private void resetTimer(){
+        timer.cancel();
+        timerRunning = false;
+        stop.setVisibility(View.INVISIBLE);
     }
 }
